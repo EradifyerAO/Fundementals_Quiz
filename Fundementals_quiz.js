3828f0c7afc8a371814f676c6e151587d82ac6e3
Zach <eradifyerao@gmail.com>
2025-08-28 10:04:30 -0700

    initial structure

---
+++ Fundementals_quiz.js
@@ -0,0 +1,6 @@
+const quizData = [
+    question 1: "What is the early basis of 3d art?",
+    options: ["Lighting", "Primatives", "UV Maps", "Materials"]
+    answer: "Primatives"
+    question 2: ""
+]
